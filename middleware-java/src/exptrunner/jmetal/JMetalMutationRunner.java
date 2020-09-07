36d5bb422c3f07195da3897018983a942034fdbc
James Harbin <james.harbin@york.ac.uk>
2020-09-07 12:04:33 +0100

    Test for jMetal representations

---
+++ middleware-java/src/exptrunner/jmetal/JMetalMutationRunner.java
@@ -0,0 +1,51 @@
+package exptrunner.jmetal;
+
+import java.util.List;
+
+import org.uma.jmetal.algorithm.Algorithm;
+import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAII;
+import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAIIBuilder;
+import org.uma.jmetal.operator.crossover.CrossoverOperator;
+import org.uma.jmetal.operator.mutation.MutationOperator;
+import org.uma.jmetal.operator.selection.SelectionOperator;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
+import org.uma.jmetal.solution.integersolution.IntegerSolution;
+import org.uma.jmetal.util.AbstractAlgorithmRunner;
+import org.uma.jmetal.util.ProblemUtils;
+import org.uma.jmetal.solution.Solution;
+
+public class JMetalMutationRunner extends AbstractAlgorithmRunner {
+	
+	private void test() {
+	    Problem<DoubleSolution> problem = ProblemUtils.<DoubleSolution>loadProblem("test.prb");
+	    Algorithm<List<DoubleSolution>> algorithm;
+	    CrossoverOperator<DoubleSolution> crossover;
+	    MutationOperator<DoubleSolution> mutation;
+	    SelectionOperator<List<DoubleSolution>, DoubleSolution> selection;
+	    
+	    String problemName = "test.prb";
+	    
+	    algorithm = new NSGAIIBuilder<DoubleSolution>(problem, crossover, mutation).build();
+	    
+	    
+//	        .setSelectionOperator(selection)
+//          .setMaxEvaluations(25000)
+//	        .setPopulationSize(100)
+//	        .build();
+	}
+	
+//    private void runMutationTest() {
+//      Problem<FaultInstanceSetSolution> problem;
+//    	Algorithm<List<FaultInstanceSetSolution>> algorithm;
+//    	CrossoverOperator<FaultInstanceSetSolution> crossover;
+//      MutationOperator<FaultInstanceSetSolution> mutation;
+//      SelectionOperator<List<FaultInstanceSetSolution>, FaultInstanceSetSolution> selection;
+        
+//    	algorithm = new NSGAIIBuilder<FaultInstanceSetSolution>(problem, crossover, mutation)
+//    			.setSelectionOperator(selection)
+//    			.setMaxEvaluations(25000)
+//    			.setPopulationSize(100)
+//    			.build() ;
+//    }
+}
