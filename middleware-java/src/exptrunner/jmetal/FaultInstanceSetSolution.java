acceddc6aabee468c6aa5dcd3245bc28e0f79355
James Harbin <james.harbin@york.ac.uk>
2020-09-07 12:08:34 +0100

    New file

--- middleware-java/src/exptrunner/jmetal/FaultInstanceSetSolution.java
+++ middleware-java/src/exptrunner/jmetal/FaultInstanceSetSolution.java
@@ -1,11 +1,114 @@
 package exptrunner.jmetal;
 
-import jmetal.core.Problem;
-import jmetal.core.SolutionType;
-import jmetal.core.Variable;
+import java.util.List;
+import java.util.Map;
 
-public class GenomeRepresentation extends SolutionType {
-	
-	
+// This is for jmetal 5
+import org.uma.jmetal.solution.*;
+import org.uma.jmetal.solution.compositesolution.*;
+
+public class FaultInstanceSetSolution implements Solution {
+
+	@Override
+	public void setObjective(int index, double value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getObjective(int index) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double[] getObjectives() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getVariable(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getVariables() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setVariable(int index, Object variable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double[] getConstraints() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getConstraint(int index) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setConstraint(int index, double value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getNumberOfVariables() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfObjectives() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfConstraints() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Solution copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setAttribute(Object id, Object value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object getAttribute(Object id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasAttribute(Object id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Map getAttributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
