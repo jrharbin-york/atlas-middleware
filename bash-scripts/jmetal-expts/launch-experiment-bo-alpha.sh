2bea4ec835783983b46892ae33431893d4cc9c94
James Harbin <james.harbin@york.ac.uk>
2020-10-28 15:13:45 +0000

    Experiment runner files

---
+++ bash-scripts/jmetal-expts/launch-experiment-bo-alpha.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner-bo-alpha.jar exptrunner.jmetal.R
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-GILDA 3.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-FRANK 3.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-HENRY 3.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-ELLA 4.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-GILDA 4.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-FRANK 4.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-HENRY 4.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-ELLA 5.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-GILDA 5.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-FRANK 5.0
+/opt/icedtea-bin-3.14.0/jre/bin/java -cp /home/jharbin/academic/atlas/atlas-middleware/expt-jar/exptrunner.jar exptrunner.RunCoverageExperiment SPEEDFAULT-HENRY 5.0
