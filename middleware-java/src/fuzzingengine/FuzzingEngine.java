1274e5c8a2bb135115bfcda9374fa24fe11793ef
James Harbin <james.harbin@york.ac.uk>
2021-03-14 06:05:10 +0000

    Changes for the install guide

--- middleware-java/src/fuzzingengine/FuzzingEngine.java
+++
@@ -1,7 +0,0 @@
-package fuzzingengine;
-
-import middleware.core.MOOSVariableUpdate;
-
-public abstract class FuzzingEngine {
-	public abstract MOOSVariableUpdate fuzzMessage(MOOSVariableUpdate mup);
-}
